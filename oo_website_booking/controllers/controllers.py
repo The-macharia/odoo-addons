2c7a8cec5800ba928c26a5b8b741f626ce991df5
Eric Macharia <rcmacharia7@gmail.com>
2021-09-14 23:04:41 +0300

    [FIX] can access homepage on / route

--- oo_website_booking/controllers/controllers.py
+++ oo_website_booking/controllers/controllers.py
@@ -2,8 +2,18 @@
 from odoo import http
 from odoo.http import request
 
+from odoo.addons.website.controllers.main import Website
+
+
+class Website(Website):
+
+    @http.route(auth='public')
+    def index(self, data={}, **kw):
+        super(Website, self).index(**kw)
+        return http.request.render('oo_website_booking.home', data)
+
 
 class OoWebsiteBooking(http.Controller):
-    @http.route('/home', type='http', csrf=True, website=True, auth='public', method='GET')
+    @http.route('/', type='http', csrf=True, website=True, auth='public', method='GET')
     def index(self, **kw):
         return request.render('oo_website_booking.home')
