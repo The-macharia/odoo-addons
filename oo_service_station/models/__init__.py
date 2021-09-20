e1a80b4f96607eccef2084736772f9c171ecd28e
Eric Macharia <rcmacharia7@gmail.com>
2021-09-20 14:02:20 +0300

    [FEAT] add modules

--- oo_service_station/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import station_sales
-from . import station_misc
-from . import station_lines
-from . import station_stations
-from . import settings
-from . import mpesa_records
--- oo_service_station/models/settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class StationSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    name = fields.Char(string='Name')
