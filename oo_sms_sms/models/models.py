ee65cac18f1e3316f1fb7324228c39e2e78dd9a9
Eric Macharia <rcmacharia7@gmail.com>
2022-02-03 20:59:46 +0300

    [FIX] update sms to use customer related contacts

--- oo_sms_sms/models/models.py
+++ oo_sms_sms/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from odoo import api, fields, models
+from odoo import fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -27,29 +27,14 @@ class AccountMove(models.Model):
     _name = 'account.move'
     _inherit = ['account.move', 'sms.oo.sms']
 
-    def prepare_sms_data(self, phone):
-        template = self.get_sms_template(self._name)
-        if template:
-            message = template.body
-            message = message.strip().format(name=self.partner_id.name,
-                                             number=self.name,
-                                             amount=self.amount_residual)
-
-            return {'to': phone, 'message': message}
-
     def post(self):
         res = super().post()
         for rec in self:
-            phone = rec.partner_id.phone or rec.partner_id.mobile
-            if phone:
-                valid_number = rec.format_and_validate_number(rec.partner_id)
-                if valid_number:
-                    message = rec.prepare_sms_data(valid_number)
-                    if message:
-                        rec.send_sms(message)
-            else:
+            numbers = rec._format_and_validate_number(rec.partner_id)
+            if not numbers:
                 raise ValidationError(
-                    "Selected customer does not have a valid phone or mobile number. \
-                    Hint: Add a country to the partner's record for better validation.")
-
+                    "Customer does not have a valid phone number. \
+                    Add a country to the customer's record for optimized validation.")
+            payload = rec.prepare_sms_data(numbers)
+            rec._send_sms(payload)
         return res
--- oo_sms_sms/models/sms.py
+++ oo_sms_sms/models/sms.py
@@ -5,25 +5,13 @@ import logging
 import requests
 from urllib.parse import urlencode
 
-from odoo import models,_
+from odoo import models, _
 from odoo.exceptions import UserError
 from odoo.addons.phone_validation.models.phone_validation_mixin import PhoneValidationMixin as PVM
 from odoo.addons.phone_validation.tools import phone_validation
+import africastalking
 
 _logger = logging.getLogger(__name__)
-
-urls = {
-    'live': "https://api.africastalking.com/version1/messaging",
-    'sandbox': "https://api.sandbox.africastalking.com/version1/messaging"
-}
-
-
-def _prepare_request_header(key):
-    return {
-        'Accept': 'application/json',
-        'apiKey': key,
-        'Content-Type': 'application/json',
-    }
 
 
 class SmsOOSms(models.AbstractModel):
@@ -32,87 +20,38 @@ class SmsOOSms(models.AbstractModel):
 
     def _get_api_settings(self):
         params = self.env['ir.config_parameter'].sudo()
-        return {
-            'username': params.get_param('oo_sms_sms.sms_username').strip(),
-            'key': params.get_param('oo_sms_sms.sms_api_key').strip(),
-            'sender': params.get_param('oo_sms_sms.sms_shortcode').strip(),
-            'env': params.get_param('oo_sms_sms.sms_api_env').strip(),
-        }
 
-    def send_sms(self, message):
-        self.ensure_one()
-        res = self._send_request(message)
-        self.prepare_post_sms_notification(partners=[self.partner_id])
-        # if res.get('Recipients'):
-        #     recipient = res.get('Recipients')[0]
-        #     self.write({
-        #         'status': recipient['status'],
-        #         'status_code': recipient['statusCode'],
-        #         'message_id': recipient['messageId'],
-        #         'json_response': recipient,
-        #         # 'state': STAT_CODE_MAP.get(recipient['statusCode'])
-        #     })
-        return True
-
-    def _send_request(self, payload):
-        if payload is None:
-            payload = {}
-        config = self._get_api_settings()
-        payload.update({
-            'username': config.get('username'),
-            'from': config.get('sender'),
-        })
-
-        if not all([config['username'], config['key'], config['sender'], config['env']]):
-            raise UserError(
-                _("Missing configuration for Africa's Talking Apis. Confirm that a Username,\
-                 Api Key, Sender and Api environment are provided."))
-        try:
-            url = urls.get(config['env']),
-            headers = _prepare_request_header(config.get('key'))
-            response = requests.post(
-                url,
-                data=urlencode(payload),
-                headers=headers
-            )
-
-            _logger.info(
-                f'Status Code: {response.status_code} - AfricasTalking returned...{response.text}')
-            response.raise_for_status()
-            response = response.json()
-
-        except Exception as e:
-            message = f'Failed sending message with error {e}'
-            _logger.error(message)
-
-        # if not isinstance(response, dict):
-        #     response = json.loads(response)
-        # response = response.get('SMSMessageData')
-        # return response
-
-    # def _send_bulk_sms(self, message):
-    #     recipients = [sms.number for sms in self]
-    #     payload = {
-    #         'to': ','.join(recipients),
-    #         'message': message,
-    #     }
-    #     res = self._send_request(payload)
-    #     for recipient in res.get('Recipients'):
-    #         sms = self.filtered(lambda s: s.number == recipient['number'])
-    #         sms.write({
-    #             'status': recipient['status'],
-    #             'status_code': recipient['statusCode'],
-    #             'message_id': recipient['messageId'],
-    #             'json_response': recipient,
-    #             # 'state': STAT_CODE_MAP.get(recipient['statusCode'], 'pending')
-    #         })
-    #     return True
+        username = params.get_param('oo_sms_sms.sms_username').strip()
+        key = params.get_param('oo_sms_sms.sms_api_key').strip()
+        sender = params.get_param('oo_sms_sms.sms_shortcode').strip()
+        return username, key, sender
 
     def get_sms_template(self, mode):
         template = self.env['sms.template'].search(
             [('model_id.model', '=', self._name)], limit=1)
         return template
 
+    def _prepare_sms_data(self, numbers):
+        template = self.get_sms_template(self._name)
+        if template:
+            message = template.body
+            message = message.strip().format(name=self.partner_id.name,
+                                             number=self.name,
+                                             amount=self.amount_residual)
+
+            return [message, numbers]
+
+    def _send_sms(self, payload):
+        username, api_key, sender = self._get_api_settings()
+        africastalking.initialize(username, api_key)
+        sms = africastalking.SMS
+
+        message, numbers = payload
+        response = sms.send(message, numbers, sender_id=sender)
+        _logger.error(response)
+        return response
+
+        # self.prepare_post_sms_notification(partners=[self.partner_id])
     def prepare_post_sms_notification(self, partners=None):
         subtype = self.env.ref('mail.mt_note')
         partners_to = False
@@ -128,14 +67,29 @@ class SmsOOSms(models.AbstractModel):
             'notified_partner_ids': partners_to,
         }
 
-    def format_and_validate_number(self, partner, company=None):
+    def _format_and_validate_number(self, partner, company=None):
+        if not partner.phone or partner.child_ids.mapped('phone'):
+            return False
+
         if not company:
             company = partner.company_id or self._context.get('company_id')
-        phone_no = PVM.phone_format(self, partner.phone,
-                                    country=partner.country_id, company=company)
+
+        numbers = [self._validate_numbers(
+            partner, company, partner.country_id)]
+
+        for child in partner.child_ids.filtered('phone'):
+            number = self._validate_numbers(child, company, partner.country_id)
+            if number:
+                numbers.append(number)
+
+        return list(set(numbers))
+
+    def _validate_numbers(self, partner, company, country, formats='E164'):
+        phone_no = PVM.phone_format(
+            partner, partner.phone, country=country, company=company)
 
         mobile_obj = phone_validation.phone_sanitize_numbers_w_record(
-            [phone_no], self, partner.country_id, force_format='E164')
+            [phone_no], country, force_format=formats)
         mobile = mobile_obj.get(phone_no)
         mobile = mobile.get('sanitized') or False
         return mobile
